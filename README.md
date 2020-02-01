0ec5d57ff36e6c46f0dbd520a01d25ed30a0c057
Kacper Koza <kacperkoza34@gmail.com>
2020-02-01 14:49:59 +0100

    Init project

---
+++ README.md
@@ -0,0 +1,11 @@
+# watermark_app
+
+# install
+
+### yarn add  or  npm install
+
+### node js is required to build and run this project
+
+# start
+
+### node app.js
---
+++ app.js
@@ -0,0 +1,107 @@
+const Jimp = require('jimp');
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
+  try {
+    const image = await Jimp.read(inputFile);
+    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+    const textData = {
+      text: text,
+      alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+      alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
+    };
+
+    image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
+
+    image.quality(100).write(outputFile);
+    
+    console.log('Success');
+  } catch (error) {
+    console.log('Something went wrong. Try again');
+    startApp();
+  }
+};
+
+const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile) {
+  try {const image = await Jimp.read(inputFile);
+    const watermark = await Jimp.read(watermarkFile);
+    const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+    const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+
+    image.composite(watermark, x, y, {
+      mode: Jimp.BLEND_SOURCE_OVER,
+      opacitySource: 0.5,
+    });
+    image.quality(100).write(outputFile);
+    console.log('Success');
+  } catch (error) {
+    console.log('Something went wrong. Try again');
+    startApp();
+  }
+};
+
+const prepareOutputFilename = (filename) => {
+  const [ name, ext ] = filename.split('.');
+  return `${name}-with-watermark.${ext}`;
+};
+
+const startApp = async () => {
+
+  // Ask if user is ready
+  const answer = await inquirer.prompt([{
+      name: 'start',
+      message: 'Hi! Welcome to "Watermark manager". Copy your image files to `/img` folder. Then you\'ll be able to use them in the app. Are you ready?',
+      type: 'confirm'
+    }]);
+
+  // if answer is no, just quit the app
+  if(!answer.start) process.exit();
+
+  // ask about input file and watermark type
+  const options = await inquirer.prompt([{
+    name: 'inputImage',
+    type: 'input',
+    message: 'What file do you want to mark?',
+    default: 'test.jpg',
+  }, {
+    name: 'watermarkType',
+    type: 'list',
+    choices: ['Text watermark', 'Image watermark'],
+  }]);
+
+  if(fs.existsSync(`./img/${options.inputImage}`)) {
+    if(options.watermarkType === 'Text watermark') {
+      const text = await inquirer.prompt([{
+        name: 'value',
+        type: 'input',
+        message: 'Type your watermark text:',
+      }])
+      options.watermarkText = text.value;
+      addTextWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), options.watermarkText);
+    }
+    else {
+      const image = await inquirer.prompt([{
+        name: 'filename',
+        type: 'input',
+        message: 'Type your watermark name:',
+        default: 'logo.png',
+      }])
+      options.watermarkImage = image.filename;
+
+      if(fs.existsSync(`./img/${options.watermarkImage}`)) {
+        addImageWatermarkToImage('./img/' + options.inputImage, './img/' + prepareOutputFilename(options.inputImage), './img/' + options.watermarkImage);
+      }
+      else {
+        console.log(`File: './img/${options.watermarkImage}' dosen\'t exist. Try again `);
+        startApp();
+      }
+    }
+  }
+  else {
+    console.log(`File: './img/${options.inputImage}' dosen\'t exist. Try again`);
+    startApp();
+  }
+};
+
+startApp();
